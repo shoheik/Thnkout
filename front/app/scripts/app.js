9aced569b955c8153f9f2195861b1fbbcfe666a5
Shohei Kameda <shoheik@cpan.org>
2013-12-07 15:36:32 +0900

    update data format

--- front/app/scripts/app.js
+++ front/app/scripts/app.js
@@ -11,7 +11,19 @@ angular.module('thnkoutApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .otherwise({
-        redirectTo: '/'
-      });
+      .when('/output', {
+        templateUrl: 'views/output.html',
+        controller: 'OutputCtrl'
+      })
+      .when('/view', {
+        templateUrl: 'views/view.html',
+        controller: 'ViewCtrl'
+      })
+      .when('/mece', {
+        templateUrl: 'views/mece.html',
+        controller: 'MeceCtrl'
+      })
+      //.otherwise({
+      //  redirectTo: '/'
+      //});
   });
--- front/app/scripts/controllers/main.js
+++ front/app/scripts/controllers/main.js
@@ -7,7 +7,104 @@ angular.module('thnkoutApp')
       'AngularJS',
       'Karma'
     ];
-
     $scope.topics = TopicGenerator.getTopics();
+  })
+  .controller('OutputCtrl', function ($scope) {
+  })
+  .controller('MeceCtrl', function ($scope) {
 
+    // This is the dataset from server
+    $scope.data = {
+      topic :{
+        name: "Become financial engineer",
+        tag: [
+          "finance",
+          "engineering"
+        ],
+        strategies: {
+          SWOT: {
+            description: "Strength, Weakness, Opportunity anad Threat",
+            approaches: {
+              strength: [
+                {
+                  user: "kamesho",
+                  thoughts: "relatively good salary",
+                  last_update: "201312071100"
+                },
+                {
+                  user: "user1",
+                  thoughts: "experience of managing a large system",
+                  last_update: "201312050800"
+                }
+              ],
+              weakness: [
+                {
+                  user: "kamesho",
+                  thoughts: "worry about the layoff during recession",
+                  last_update: "201312071100"
+                }
+              ],
+              opportunity:[
+                {
+                  user: "kamesho",
+                  thoughts: "a few but it can be if you wish",
+                  last_update: "201312071100"
+                }
+              ],
+              threat:[
+                {
+                  user: "kamesho",
+                  thoughts: "totally depending on the economy",
+                  last_update: "201312071100"
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+
+    $scope.strategies = [
+      {
+        name: "5W1H",
+      },
+      {
+        name: "SWOT",
+      }
+    ];
+
+    $scope.desc = {
+      "SWOT": "Strength, Weakness, Opportunity anad Threat",
+      "5W1H":  "What Where When Who Why and How"
+    }
+
+    var group = {
+      "SWOT": ["Strength", "Weakness" , "Opportunity", "Threat"],
+      "5W1H": ["What", "Where", "When", "Who", "How"]
+    }
+
+    $scope.selectStrategy = function() {
+      //alert($scope.strategy_name);
+      $scope.angles = group[$scope.strategy_name];
+    };
+
+    $scope.issues = ["issue1", "issue2"];
+    $scope.output = {
+      "Strenth": []
+    };
+
+    // need who and where
+    $scope.send = function (output, strategy, approach){
+      var idea ={};
+      idea.user = "kamesho";
+      var d = new Date();
+      idea.last_update = d.toString();
+      idea.thoughts = output;
+      console.log($scope.data.topic.strategies[strategy].approaches[approach]);
+      $scope.data.topic.strategies[strategy].approaches[approach].push(idea);
+    }
+
+  })
+  .controller('ViewCtrl', function ($scope, TopicGenerator) {
+    $scope.topics = TopicGenerator.getTopics();
   });
