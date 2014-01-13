9da46bc7a4ea4dd2decc09ad234ca642f06171de
Shohei Kameda <shoheik@cpan.org>
2014-01-13 16:52:33 +0900

    update infra - docker config

---
+++ infra/docker/mysql/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PASS='3dDd9d983dafdmvznc9'
+DB='thnkout_test'
+
+/etc/init.d/mysqld start &
+/usr/bin/mysqladmin -u root password $PASS 
+/usr/bin/mysql -u root -p$PASS -e"CREATE DATABASE $DB"
+/usr/bin/mysql -u root -p$PASS -e"grant all privileges on thnkout_test.* to thnkout@'172.17.%.%' identified by 'thnkout'";
+
